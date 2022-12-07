b16ee98a0b90e14029168e78b8d9a9a38a3ac4b9
lOlmrtn <lroutschek@gmail.com>
2022-12-07 15:06:04 +0100

    Update morsecode.py

--- morsecode/morsecode.py
+++ morsecode/morsecode.py
@@ -2,9 +2,11 @@
 from time import sleep
 from machine import Pin
                                                 
-led = Pin(25, Pin.OUT)
+led_red = Pin(14, Pin.OUT)
+led_green = Pin(15, Pin.OUT)
+condition = True
 
-letters: dict = {"A": [0,1], "B": [1,0,0,0], "C": [1,0,1,0], "D": [1,0,0], "E": [0], "F": [0,0,1,0], "G": [1,1,0],
+letters: dict =    {"A": [0,1], "B": [1,0,0,0], "C": [1,0,1,0], "D": [1,0,0], "E": [0], "F": [0,0,1,0], "G": [1,1,0],
                     "H": [0,0,0,0], "I": [0,0], "J": [0,1,1,1], "K": [1,0,1], "L": [0,1,0,0], "M": [1,1], "N": [1,0],
                     "O": [1,1,1], "P": [0,1,1,0], "Q": [1,1,0,1], "R": [0,1,0], "S": [0,0,0], "T": [1], "U": [0,0,1],
                     "V": [0,0,0,1], "W": [0,1,1], "X": [1,0,0,1], "Y": [1,0,1,1], "Z": [1,1,0,0], " ": [2],
@@ -21,22 +23,24 @@ def checkString(Str):
 
 def greenLightOn():       # 0 --> short;
     print("Green on!")
-    led.value(1)
+    led_green.value(1)
     sleep(0.1)
-    led.value(0)
-
+    led_green.value(0)
+    sleep(0.2)
 
 def redLightOn():         # 1-->long
     print("Red on!")
-    led.value(1)
+    led_red.value(1)
     sleep(0.5)
-    led.value(0)
+    led_red.value(0)
+    sleep(0.2)
 
 
 word = input("Was möchten Sie ausgeben? \n")
 word = word.upper() 
 
-    
+while condition==True:
+
 
 for char in word:
     print(letters[char])
