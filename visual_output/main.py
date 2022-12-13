33813c9ad8e1911343e40a662290776d0edb0489
henningf3 <henning.dierks03@gmail.com>
2022-12-13 10:57:09 +0100

    fertig konsole

--- visual_output/main.py
+++ visual_output/main.py
@@ -1,35 +1,13 @@
 """
 Pico - Morse Code
-    - web-parts inspired by code shown in lecture
 """
 
 from time import sleep
 from machine import Pin
-import network
-from socket import Socket
-import sys
 
+global executing
+executing=False
 
-def setup():
-    wap = network.WLAN(network.AP_IF)
-    wap.config(essid=SSID, password=PASS)
-    wap.active(True)
-
-    ip = wap.ifconfig()[0]
-
-    # give user IP and PORT (to enter in Browser and access UI)
-    print("IP-Address: " + str(ip))
-    print("Port: " + str(PORT))
-
-    # init Socket
-    sock = socket.socket()
-    sock.bind((ip, PORT))
-    sock.open()
-    print(sock)
-    return sock
-
-
-    
 # morse function
 def morseOutput(word):
     for char in word:
@@ -38,11 +16,14 @@ def morseOutput(word):
 
 
 # check validity of string
-def checkString(word: str):
-    if word.isalpha():
-        return True
-    else:
-        return False
+def validInput():
+    while True:
+        word=input("What do you want to send?").replace(" ","").upper()
+        if word.isalpha():
+            return word
+        else:
+            print("Input not valid.")
+            lightError()
 
 
 # LED functions
@@ -67,13 +48,15 @@ def lightError():
     sleep(3)
     led_red.value(0)
 
+def handle_interrupt(pin):
+    global executing  
+    executing = not executing
 
 # init LED
 led_red = Pin(14, Pin.OUT)
 led_green = Pin(15, Pin.OUT)
 button = Pin(16, Pin.IN)
-
-
+button.irq(trigger=Pin.IRQ_RISING, handler=handle_interrupt)
 
 # init Dict
 letters: dict = {
@@ -108,103 +91,14 @@ letters: dict = {
     "Ü": [short, short, long, long],
     " ": [wait],
 }
-
-# constants for web - interface
-SSID = "WAP-Morsecode"
-PASS = "PicoPi123"
-PORT = 80
-WEBSITE = """<!DOCTYPE html>
-<html>
-<body>
-<h2>MorseCode - Input</h2>
-<form>
-  <label>Was last text valid Input? %s</label><br>
-  <label>Last entered text: %s</label><br>
-  <label>Enter text:</label><br>
-  <input type="text" id="input" name="input" value=" "><br>
-  <input type="submit" value="Submit">
-</form> 
-</body>
-</html>
-"""
-
-
-
+    
 def main():
     # init UI-vars
-    lastText = ""
-    lastValid = False
-
     while True:
-        try:
-            conn, addr = sock.accept()
-            print("Connected to: " + str(addr))
-        except socket.error:
-            print("Socket error, exiting.")
-            break
-        try:
-            request = conn.recv(1024)
-            reqText = str(request)
-
-            # if input is submitted (else the find returns -1)
-            if reqText.find("/?input=") + 1:
-
-                # extract the input from recieved
-                tempText = reqText[
-                    (reqText.find("/?input=") + 9) : (reqText.find(" HTTP/1.1"))
-                ]
-
-                # a bit of manipulation (no spaces... implemented)
-                check_tempText = tempText.replace("+", "")
-                print(check_tempText)
-                lastValid = checkString(check_tempText)
-                print(lastValid)
-
-                if lastValid:
-                    lastText = tempText.replace("+", " ")
-                    lastText = tempText.upper()
-                else:
-                    lastText = ""
-
-            # formatting website
-            html = WEBSITE % (str(lastValid), lastText)
-
-            conn.send(
-                "HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n"
-            )  # HTTP status line and header
-            conn.send(html)
-            conn.close()
-            
-            # the LAST VALID text is morsed
-            if lastValid:
-                morseOutput(lastText)
-            else:
-                lightError()
-
-        except OSError:
-            conn.close()
-            print("Error occured")
-
-executing = False
-
-def handle_interrupt(pin,sock):
-    global executing
-    executing = not executing
-
-    if executing:
-        print(sock)
-        #sock.open()
-        sock.listen()
-        main()
-        print('main()')
-    else:
-        sock.detach()
-        print('wap not active')
-
-if __name__ == "__main__":
-    sock = setup()
-
-    button.irq(trigger=Pin.IRQ_RISING, handler=lambda pin=None, sock=sock:handle_interrupt(pin,sock))
-
-    while True:
-        sleep(0.001)
+        if executing:
+            sleep(3)
+        else:
+            validText = validInput()
+            morseOutput(validText)
+        
+main()
