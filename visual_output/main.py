17b83ba7147876698717f355ebbd89934bf806a5
Niklas Weber <niklas.weber2@mailbox.tu-dresden.de>
2022-12-13 13:28:05 +0100

    fixxed web ui

--- visual_output/main.py
+++ visual_output/main.py
@@ -3,31 +3,11 @@ Pico - Morse Code
     - web-parts inspired by code shown in lecture
 """
 
-from time import sleep
+from time import sleep, time
 from machine import Pin
 import network
-from socket import Socket
+import socket
 import sys
-
-
-def setup():
-    wap = network.WLAN(network.AP_IF)
-    wap.config(essid=SSID, password=PASS)
-    wap.active(True)
-
-    ip = wap.ifconfig()[0]
-
-    # give user IP and PORT (to enter in Browser and access UI)
-    print("IP-Address: " + str(ip))
-    print("Port: " + str(PORT))
-
-    # init Socket
-    sock = socket.socket()
-    sock.bind((ip, PORT))
-    sock.open()
-    print(sock)
-    return sock
-
 
     
 # morse function
@@ -115,20 +95,35 @@ PASS = "PicoPi123"
 PORT = 80
 WEBSITE = """<!DOCTYPE html>
 <html>
-<body>
+<head>
+<style>
+#parent{
+display: flex;
+justify-content: center;
+align-items: center;
+}
+body {
+  min-height: 100vh;
+  padding: 0;
+}
+</style>
+</head>
+<body id="parent">
 <h2>MorseCode - Input</h2>
 <form>
+  <label> Service currently %s</label><br> 
   <label>Was last text valid Input? %s</label><br>
   <label>Last entered text: %s</label><br>
   <label>Enter text:</label><br>
   <input type="text" id="input" name="input" value=" "><br>
   <input type="submit" value="Submit">
-</form> 
+</form>
 </body>
 </html>
 """
 
 
+executing = False
 
 def main():
     # init UI-vars
@@ -136,12 +131,11 @@ def main():
     lastValid = False
 
     while True:
-        try:
-            conn, addr = sock.accept()
-            print("Connected to: " + str(addr))
-        except socket.error:
-            print("Socket error, exiting.")
-            break
+
+        conn, addr = sock.accept()
+        print("Connected to: " + str(addr))
+
+            
         try:
             request = conn.recv(1024)
             reqText = str(request)
@@ -156,18 +150,19 @@ def main():
 
                 # a bit of manipulation (no spaces... implemented)
                 check_tempText = tempText.replace("+", "")
-                print(check_tempText)
                 lastValid = checkString(check_tempText)
-                print(lastValid)
 
                 if lastValid:
                     lastText = tempText.replace("+", " ")
-                    lastText = tempText.upper()
+                    lastText = lastText.upper()
                 else:
                     lastText = ""
 
             # formatting website
-            html = WEBSITE % (str(lastValid), lastText)
+            if executing:
+                html = WEBSITE % (f"<font color=\"green\"><b>available</b></font>", str(lastValid), lastText)
+            else:
+                html = WEBSITE % (f"<font color=\"firebrick\"><b>unavailable</b></font>", str(lastValid), lastText)
 
             conn.send(
                 "HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n"
@@ -176,35 +171,44 @@ def main():
             conn.close()
             
             # the LAST VALID text is morsed
-            if lastValid:
-                morseOutput(lastText)
-            else:
-                lightError()
+            if lastValid and executing:
+                morseOutput(lastText)                
+                
 
         except OSError:
             conn.close()
             print("Error occured")
 
-executing = False
+last_interrupt = time()
+def handle_interrupt(pin):
+    global last_interrupt
+    current_time = time()
+    print(f"Difference: {current_time-last_interrupt}")
+    if current_time-last_interrupt >= 2:
+        last_interrupt = current_time
+        print(f"last_interrupt: {last_interrupt}")
 
-def handle_interrupt(pin,sock):
-    global executing
-    executing = not executing
+        global executing
+        executing = not executing
+        print(f"Executing: {executing}")
 
-    if executing:
-        print(sock)
-        #sock.open()
-        sock.listen()
-        main()
-        print('main()')
-    else:
-        sock.detach()
-        print('wap not active')
+wap = network.WLAN(network.AP_IF)
+wap.config(essid=SSID, password=PASS)
+wap.active(True)
+
+ip = wap.ifconfig()[0]
+
+# give user IP and PORT (to enter in Browser and access UI)
+print("IP-Address: " + str(ip))
+print("Port: " + str(PORT))
+
+# init Socket
+sock = socket.socket()
+sock.bind((ip, PORT))
+sock.listen()
+print(sock)
 
 if __name__ == "__main__":
-    sock = setup()
+    button.irq(trigger=Pin.IRQ_RISING, handler=handle_interrupt)
 
-    button.irq(trigger=Pin.IRQ_RISING, handler=lambda pin=None, sock=sock:handle_interrupt(pin,sock))
-
-    while True:
-        sleep(0.001)
+    main()
